d2aa22d5b8e4cdbda0854d1864516abeaaf91998
martinbucca <mbucca@fi.uba.ar>
2023-08-06 22:55:45 -0300

    add all files

---
+++ constants.py
@@ -0,0 +1,10 @@
+WIN_NUMBER = 2048
+POSSIBLE_INITIAL_VALUES = (2, 4)
+ROWS = 4
+COLUMNS = 4
+UP = 'Up'
+RIGHT = 'Right'
+DOWN = 'Down'
+LEFT = 'Left'
+EMPTY = 0
+COLORS = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563", 32: "#f67c5f", 64: "#f65e3b", 128: "#edcf72", 256: "#edcc61", 512: "#edc850", 1024: "#edc53f", 2048: "#edc22e"}
---
+++ main.py
@@ -0,0 +1,41 @@
+import gamelib as gamelib
+import logic
+from constants import UP, DOWN, LEFT, RIGHT
+
+'''
+def main():
+    juego = logica.inicializar_juego()
+    while True:
+        logica.mostrar_juego(juego)
+        if logica.juego_ganado(juego):
+            print("Felicidades! Sos lo mas")
+            return
+        if logica.juego_perdido(juego):
+            print("Lola, volve a intentar")
+            return
+        dir = logica.pedir_direccion(juego)
+        nuevo_juego = logica.actualizar_juego(juego, dir)
+        if nuevo_juego != juego:
+            juego = logica.insertar_nuevo_random(nuevo_juego)
+'''
+
+def main():
+    gamelib.resize(800, 500)
+    gamelib.title("2048")
+    gamelib.draw_rectangle(0, 0, 800, 500, fill="white")
+    board = logic.initialize_2048()
+    while gamelib.is_alive():
+        logic.show_ui(board)
+        ev = gamelib.wait(gamelib.EventType.KeyPress)
+        if not ev:
+            break
+        print(board)
+        updated_board = logic.update_game(board, ev.key)
+        print(updated_board)
+        if updated_board != board:
+            board = logic.insert_random_new_cell(updated_board)
+
+        
+
+    
+gamelib.init(main)
