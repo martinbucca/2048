51dfc9007a40ddf48db3ef65ab3943db97159057
martinbucca <mbucca@fi.uba.ar>
2023-08-07 00:49:43 -0300

    refactor: constants, class Game

--- logic.py
+++ logic.py
@@ -3,179 +3,169 @@ import gamelib as gamelib
 from constants import *
 
 
-
-
-
-
-def initialize_2048():
-    ''' 
-    Initializes the game board with a all the cells empty and a random cell with a value from the possible initial values.
-    '''
-    initial_board = [[EMPTY for j in range(COLUMNS)] for i in range(ROWS)]
-    initial_row, initial_column = get_random_empty_cell(initial_board)
-    initial_board[initial_row][initial_column] = random.choice(POSSIBLE_INITIAL_VALUES)
-    return initial_board
-
-
-def show_ui(board):
-    ''' 
-    Shows the game board on the screen.
-    '''
-    for i in range(ROWS):
-        for j in range(COLUMNS):
-            x1, y1 = BOARD_X_MARGIN + j * CELL_SIZE, BOARD_Y_MARGIN + i * CELL_SIZE
-            x2, y2 = x1 + CELL_SIZE, y1 + CELL_SIZE
-            value = board[i][j]
-            gamelib.draw_rectangle(x1, y1, x2, y2, fill=COLORS.get(value, DEFAULT_CELL_COLOR))
-            if value != EMPTY:
-                gamelib.draw_text(str(value), (x1 + x2) / 2, (y1 + y2) / 2, fill="black")
-
-
-
-
-def no_vertical_moves(board):
-    '''
-    return True if there are vertical moves possible, False otherwise.
-    '''
-    return possible_horizontal_moves(transpose_board(board))
-
-def possible_horizontal_moves(board):
-    '''
-    Returns True if there are horizontal moves possible, False otherwise.
-    '''
-    for i in range(ROWS):
-        if possible_moves_in_row(board[i]):
+class Game:
+    def __init__(self):
+        ''' 
+        Initializes the game board with a all the cells empty and a random cell with a value from the possible initial values.
+        '''
+        self.board = [[EMPTY for j in range(COLUMNS)] for i in range(ROWS)]
+        self.insert_random_new_cell()
+        self.score = INITIAL_SCORE
+    def show(self):
+        ''' 
+        Shows the game board on the screen.
+        '''
+        gamelib.draw_rectangle(0, 0, 800, 600, fill=BACKGROUND_COLOR)
+        gamelib.draw_text("Score", SCORE_LABEL_POSITION[0], SCORE_LABEL_POSITION[1], size=30, fill="white")
+        gamelib.draw_text(str(self.score), SCORE_POSITION[0], SCORE_POSITION[1], size=30, fill="white")
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                x1, y1 = BOARD_X_MARGIN + j * CELL_SIZE, BOARD_Y_MARGIN + i * CELL_SIZE
+                x2, y2 = x1 + CELL_SIZE, y1 + CELL_SIZE
+                value = self.board[i][j]
+                gamelib.draw_rectangle(x1, y1, x2, y2, fill=COLORS.get(value, DEFAULT_CELL_COLOR))
+                if value != EMPTY:
+                    gamelib.draw_text(str(value), (x1 + x2) / 2, (y1 + y2) / 2, fill=NUMBERS_COLOR)
+    def get_random_empty_cell(self):
+        '''
+        Returns a random empty cell from the board. If there are no empty cells, returns False.
+        '''
+        empty_cells = [(i, j) for i in range(ROWS) for j in range(COLUMNS) if self.board[i][j] == EMPTY]
+        return random.choice(empty_cells) if empty_cells else False
+    def insert_random_new_cell(self):
+        '''
+        Inserts a new cell with a value from the possible initial values in a random empty cell of the board.
+        '''
+        row, column = (self.get_random_empty_cell())
+        self.board[row][column] = random.choice(POSSIBLE_INITIAL_VALUES)
+    def move_left(self):
+        '''
+        Moves the board to the left, combining the numbers that can be combined and moving them to the left.
+        '''
+        changed = False
+        for row in range(ROWS):
+            if self.combine_row(row):
+                changed = True
+        return changed
+    def combine_row(self, row_index):
+        '''
+        Combines the numbers of a line that can be combined and moves them to the left.
+        If there are no combinable numbers, returns the original line.
+        '''
+        row = self.board[row_index]
+        combinable_numbers = [number for number in row if number != EMPTY]
+        if combinable_numbers:
+            result = []
+            i = 0
+            while i < len(combinable_numbers):
+                if i < len(combinable_numbers) - 1 and combinable_numbers[i] == combinable_numbers[i + 1]:
+                    result.append(combinable_numbers[i] * 2)
+                    self.score += combinable_numbers[i] * 2
+                    i += 2
+                else:
+                    result.append(combinable_numbers[i])
+                    i += 1
+            empty_cells_to_add = len(row) - len(result)
+            result += [EMPTY] * empty_cells_to_add
+            self.board[row_index] = result
+            return result != row
+        return False
+    def move_right(self):
+        '''
+        Moves the board to the right, combining the numbers that can be combined and moving them to the right.
+        '''
+        self.invert_board()
+        changed = self.move_left()
+        self.invert_board()
+        return changed
+    def invert_board(self):
+        '''
+        Inverts the rows of the board.
+        '''
+        self.board = [list(reversed(row)) for row in self.board]
+    def transpose_board(self):
+        '''
+        Transposes the rows and columns of the board.
+        '''
+        self.board = [[row[i] for row in self.board] for i in range(COLUMNS)]
+    def move_up(self):
+        '''
+        Moves the board to the up, combining the numbers that can be combined and moving them to the up.
+        '''
+        self.transpose_board()
+        changed = self.move_left()
+        self.transpose_board()
+        return changed
+    def move_down(self):
+        '''
+        Moves the board to the down, combining the numbers that can be combined and moving them to the down.
+        '''
+        self.transpose_board()
+        changed = self.move_right()
+        self.transpose_board()
+        return changed
+    def update(self, direction):
+        '''Updates the game according to the specified movement in the desired direction to move the board.'''
+        changed = False
+        if direction == UP:
+            changed = self.move_up()
+        elif direction == DOWN:
+            changed = self.move_down()
+        elif direction == LEFT:
+            changed = self.move_left()
+        elif direction == RIGHT:
+            changed = self.move_right()
+        if changed:
+            self.insert_random_new_cell()
+    def won(self):
+        '''
+        Returns True if the game is won, False otherwise.
+        '''
+        for i in range(ROWS):
+            if WIN_NUMBER in self.board[i]:
+                return True
+        return False
+    def lost(self):
+        '''
+        Returns True if the game is lost, False otherwise.
+        '''
+        if not self.possible_horizontal_moves() and not self.possible_horizontal_moves(self.transpose_board()):
             return True
-    return False
-
-def possible_moves_in_row(row):
-    '''
-    Returns True if there are possible moves in the row, False otherwise.
-    '''
-    i = 0
-    while i < len(row):
-        if row[i] == EMPTY:
-            return True
-        if i < len(row) - 1 and row[i] == row[i + 1]:
-            return True
-        i += 1
-    return False
-
-
-
-def won_game(board):
-    '''
-    Returns True if the game is won, False otherwise.
-    '''
-    for i in range(ROWS):
-        if WIN_NUMBER in board[i]:
-            return True
-    return False
-
-
-def lost_game(board):
-    '''
-    Returns True if the game is lost, False otherwise.
-    '''
-    if not possible_horizontal_moves(board) and not possible_horizontal_moves(transpose_board(board)):
-        return True
-    return False
+        return False
+    def possible_horizontal_moves(self):
+        '''
+        Returns True if there are horizontal moves possible, False otherwise.
+        '''
+        for i in range(ROWS):
+            if self.possible_moves_in_row(self.board[i]):
+                return True
+        return False
+    def possible_moves_in_row(self, row):
+        '''
+        Returns True if there are possible moves in the row, False otherwise.
+        '''
+        i = 0
+        while i < len(row):
+            if row[i] == EMPTY:
+                return True
+            if i < len(row) - 1 and row[i] == row[i + 1]:
+                return True
+            i += 1
+        return False
     
 
-def invert_board(board):
-    '''
-    Inverts the rows of the board.
-    '''
-    return [list(reversed(row)) for row in board]
 
-def transpose_board(board):
-    '''
-    Transposes the rows and columns of the board.
-    '''
-    transposed_board = [[row[i] for row in board] for i in range(COLUMNS)]
-    return transposed_board
 
-def combine_line(line):
-    '''
-    Combines the numbers of a line that can be combined and moves them to the left.
-    If there are no combinable numbers, returns the original line.
-    '''
-    combinable_numbers = [number for number in line if number != EMPTY]
-    if not combinable_numbers:
-        return line
-    result = []
-    i = 0
-    while i < len(combinable_numbers):
-        if i < len(combinable_numbers) - 1 and combinable_numbers[i] == combinable_numbers[i + 1]:
-            result.append(combinable_numbers[i] * 2)
-            i += 2
-        else:
-            result.append(combinable_numbers[i])
-            i += 1
 
-    # Añadir ceros al final hasta completar el largo de la fila original
-    empty_cells_to_add = len(line) - len(result)
-    result += [EMPTY] * empty_cells_to_add
-    return result
 
-def move_left(board):
-    '''
-    Moves the board to the left, combining the numbers that can be combined and moving them to the left.
-    '''
-    moved_board = board.copy()
-    for i in range(ROWS):
-        moved_board[i] = combine_line(moved_board[i])
-    return moved_board
 
-def move_up(board):
-    '''
-    Moves the board to the up, combining the numbers that can be combined and moving them to the up.
-    '''
-    transposed_board = transpose_board(board)
-    moved_board = move_left(transposed_board)
-    return transpose_board(moved_board)
 
-def move_down(board):
-    '''
-    Moves the board to the down, combining the numbers that can be combined and moving them to the down.
-    '''
-    transposed_board = transpose_board(board)
-    moved_board = move_right(transposed_board)
-    return transpose_board(moved_board)
 
-def move_right(board):
-    '''
-    Moves the board to the right, combining the numbers that can be combined and moving them to the right.
-    '''
-    inverted_board = invert_board(board)
-    moved_board = move_left(inverted_board)
-    return invert_board(moved_board)
 
-def update_game(board, direction):
-    '''Updates the game according to the specified movement in the desired direction to move the board.'''
-    if direction == UP:
-        return move_up(board)
-    elif direction == DOWN:
-        return move_down(board)
-    elif direction == LEFT:
-        return move_left(board)
-    elif direction == RIGHT:
-        return move_right(board)
 
-def get_random_empty_cell(board):
-    '''
-    Returns a random empty cell from the board. If there are no empty cells, returns False.
-    '''
-    empty_cells = [(i, j) for i in range(ROWS) for j in range(COLUMNS) if board[i][j] == EMPTY]
-    return random.choice(empty_cells) if empty_cells else False
 
-def insert_random_new_cell(board):
-    '''
-    Inserts a new cell with a value from the possible initial values in a random empty cell of the board.
-    '''
-    row, column = (get_random_empty_cell(board))
-    board[row][column] = random.choice(POSSIBLE_INITIAL_VALUES)
-    return board
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 import gamelib as gamelib
-import logic
+from logic import Game
 from constants import UP, DOWN, LEFT, RIGHT, BACKGROUND_COLOR
 
 
 def main():
     gamelib.resize(800, 600)
     gamelib.title("2048")
-    gamelib.draw_rectangle(0, 0, 800, 600, fill=BACKGROUND_COLOR)
-    gamelib.draw_text("Score", 400, 20, size=30, fill="white")
-    board = logic.initialize_2048()
+    game = Game()
     while gamelib.is_alive():
-        if logic.won_game(board):
+        if game.won():
             gamelib.say("You won!")
             break
-        if logic.lost_game(board):
+        if game.lost():
             gamelib.say("You lost!")
             break
-        logic.show_ui(board)
+        game.show()
         ev = gamelib.wait(gamelib.EventType.KeyPress)
         if not ev:
             break
-        updated_board = logic.update_game(board, ev.key)
-        if updated_board != board:
-            board = logic.insert_random_new_cell(updated_board)
+        game.update(ev.key)
 
         
 gamelib.init(main)
